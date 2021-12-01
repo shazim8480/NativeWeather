b10c4678de882361ae57b99f16766971de670025
shazim8480 <troublmaker8480@gmail.com>
2021-12-01 17:16:48 +0600

    native

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* city container */}
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Dhaka</Text>
+      </View>
+      {/* weather container */}
+      <View style={styles.weather}>
+        {/* day container */}
+        <View style={styles.day}>
+          <Text style={styles.temp}>25</Text>
+          <Text style={styles.description}>Clear</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +24,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cityName: {
+    marginTop: 40,
+    fontWeight: "500",
+    fontSize: 60,
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
   },
 });
